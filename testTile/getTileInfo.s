b119466aaa80283d8489d7fb3a308075b4445808
Sr. Smurf <gabriel_sousilva@yahoo.com.br>
2014-12-05 06:00:35 -0300

    funcao getTileInfo consertada

--- testTile/getTileInfo.s
+++ testTile/getTileInfo.s
@@ -1,15 +1,16 @@
 #Dado um endereço de memória de uma matriz de movimentação, retornar o conteudo de uma posicao dessa matriz
 #$v0 -> conteudo da posicao da matriz
 #$a0 -> começo da matriz
-#$a1 -> linha da matriz - 1
-#$a2 -> coluna da matriz - 1
+#$a1 -> x
+#$a2 -> y
 
 .text
-FUNCAO:
-	addi $t0, $zero, 4 #$t0 = 4
-	mul $t0, $a1, $t0 #$t0 = 4*L
-	add $t0, $t0, $a2 #$t0 = 4L + C (numero do byte a ser acessado)
+getTileInfo:
+	addi $t0, $zero, 16 
+	mult $a2, $t0
+	mflo $t0
+	add $t0, $t0, $a1 
 	
-	add $t0, $a0, $t0 #$t0 = posicao inicial da matriz + o que deve deslocar em bytes
+	add $t0, $a0, $t0 
 	lb $v0, 0($t0) #carrega em $v0 o conteudo desejado
 	jr $ra
