cd2d80e2be174f0ddf6f08cc422fd94bde7d68b0
Sinayra <sinayra@outlook.com>
2014-12-05 18:20:28 -0200

    Protótipo das funções do Max

---
+++ jogo/modulos/max.s
@@ -0,0 +1,23 @@
+.text
+
+#############
+# $a0: posição (i,j) do Max atual
+#
+############
+apagaMax:
+
+#############
+# $a0: posição (i,j) do Max atual
+#
+############
+verificaMoveMax:
+
+#############
+# $a0: posição (i,j) do Max atual
+############
+moveMax:
+
+#############
+# $a0: posição (i,j) do Max atual
+############
+printaMax:
